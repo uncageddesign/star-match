a04d6d69fd4ae2899c35102e9776ee2a083bbf65
uncageddesign <mhairimccrindle@gmail.com>
2021-03-24 15:25:59 +0000

    basic game from tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Game from './Components/Game';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+};
 
 export default App;
